67d0aa77c56dbe0dabcf12e09e7df05f5d0c60ef
VikkyNur <157639668+VikkyNur@users.noreply.github.com>
2025-12-02 08:32:55 -0500

    TC_01.002.01 | New Item > Folder > Create a New Folder (#226)
    * add test_create_new_folder_vn.py
    
    * amend the test_create_new_folder_vn.py

---
+++ tests/test_create_new_folder_vn.py
@@ -0,0 +1,27 @@
+import random
+
+from playwright.sync_api import expect
+
+
+def test_TC_01_002_01(page):
+    page.goto("/")
+
+    new_folder_name = f"Test_folder_{random.randint(0, 999999)}"
+
+    new_item_link_loc = "a[href='/view/all/newJob']"
+
+    item_name_field_loc = "input[class='jenkins-input']"
+    item_type_text = "Folder"
+    ok_btn_loc = "button[id='ok-button']"
+    save_btn_loc = "button[name='Submit']"
+    folder_title_loc = "h1.job-index-headline"
+
+    page.locator(new_item_link_loc).click()
+    page.locator(item_name_field_loc).fill(new_folder_name)
+    page.get_by_text(item_type_text, exact=True).click()
+    page.locator(ok_btn_loc).click()
+
+    page.locator(save_btn_loc).click()
+
+    folder_title = page.locator(folder_title_loc)
+    expect(folder_title).to_have_text(new_folder_name)
