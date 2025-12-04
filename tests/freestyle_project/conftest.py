e8e118b3582679a994fc2700a7718c065fa4057d
pavel pashchenkov <ppashchenkov@gmail.com>
2025-12-04 22:46:59 +0300

    add wait_fot_url to fixture Freestyl

--- tests/freestyle_project/conftest.py
+++ tests/freestyle_project/conftest.py
@@ -10,6 +10,7 @@ def freestyle(page):
     page.get_by_text("Enter an item name").fill(Freestyle.project_name)
     page.get_by_text(Freestyle.freestyle_type_text).click()
     page.get_by_text('OK').click()
+    page.wait_for_url(f'/job/{Freestyle.project_name}/configure')
     yield page
     page.get_by_text("Save").click()
     page.get_by_text("Delete Project").click()
