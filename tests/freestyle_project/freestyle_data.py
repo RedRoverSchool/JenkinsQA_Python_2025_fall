31b5363dc85da20ea82aa4310ae3c58a814bc27b
ppashchenkov <email@example.com>
2025-12-04 13:57:08 +0300

    TC_02.005.02 | Freestyle Project > Environment > checkboxes can be selected

--- tests/freestyle_project/freestyle_data.py
+++ tests/freestyle_project/freestyle_data.py
@@ -6,3 +6,11 @@ class Freestyle:
     freestyle_type_text = """Classic, general-purpose job type that checks out from up to one SCM, 
                             executes build steps serially, followed by post-build steps like archiving artifacts 
                             and sending email notifications."""
+    expected_list_of_environment_checkboxes = [
+        'Delete workspace before build starts',
+        'Use secret text(s) or file(s)',
+        'Add timestamps to the Console Output',
+        'Inspect build log for published build scans',
+        "Terminate a build if it's stuck",
+        'With Ant'
+    ]
