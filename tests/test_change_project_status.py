c1392a9603df959b6c673680c20ccc017b33b31a
Ekaterina <benibarker394@gmail.com>
2025-12-04 20:38:10 +0300

    TС_03_001_01 | Change Project Status (#246)
    * add_test_TC_03_001_01
    
    * edit_test_TC_03_001_01
    
    * edit_test_TC_03_001_01_change_project_status

---
+++ tests/test_change_project_status.py
@@ -0,0 +1,43 @@
+import random
+
+
+def test_TC_03_001_01(page):
+    page.goto("/")
+
+    new_item_name = f"katerina-{random.randint(1, 9999)}"
+    """Локаторы страницы"""
+    new_item_btn_loc = 'a[href="/view/all/newJob"]'
+    item_name_field_loc = 'input[class="jenkins-input"]'
+    item_type_text = "Pipeline"
+    ok_btn_loc = 'button[id="ok-button"]'
+    logo_loc = 'img[id="jenkins-head-icon"]'
+    created_item_loc = lambda name: f"td > a[href='job/{name}/']"
+
+    """Страница добавления item"""
+    page.locator(new_item_btn_loc).click()
+    page.locator(item_name_field_loc).fill(new_item_name)
+    page.get_by_text(item_type_text, exact=True).click()
+    page.locator(ok_btn_loc).click()
+    """Возвращение на главную"""
+    page.locator(logo_loc).click()
+
+    toggle_loc = 'label[for="enable-disable-project"]'
+    save_btn_loc = 'button[formnovalidate="formNoValidate"]'
+    main_page_loc = 'span[class="jenkins-mobile-hide"]'
+
+    item_link = page.locator('td > a[href^="job/"]').last
+    new_item_name = item_link.text_content()
+
+    status_icon_loc = page.locator(f'tr#job_{new_item_name} svg[tooltip]').nth(0)
+    before_status = status_icon_loc.get_attribute("tooltip")
+
+    page.get_by_role("link", name=new_item_name, exact=True).click()
+    page.get_by_role("link", name="Configure").click()
+    page.locator(toggle_loc).click()
+    page.locator(save_btn_loc).click()
+    page.locator(main_page_loc).click()
+
+    status_icon_loc = page.locator(f'tr#job_{new_item_name} svg[tooltip]').nth(0)
+    after_status = status_icon_loc.get_attribute("tooltip")
+
+    assert before_status != after_status
