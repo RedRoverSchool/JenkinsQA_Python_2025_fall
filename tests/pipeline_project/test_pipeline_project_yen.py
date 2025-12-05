232289a18350bba3e31ea1d2bbc53ba5cf64bdc8
YevgenyN <163114876+tstyev@users.noreply.github.com>
2025-12-05 22:30:09 +0300

    TC_03.002.01 (#261)
    #260
    Add test_add_description_pipeline_project

--- tests/pipeline_project/test_pipeline_project_yen.py
+++ tests/pipeline_project/test_pipeline_project_yen.py
@@ -20,3 +20,23 @@ def test_create_pipeline_project(page, delete_project):
     page.get_by_role("link", name="Jenkins", exact=True).click()
 
     expect(page.get_by_role("link", name=project_name, exact=True)).to_be_visible()
+
+def test_add_description_pipeline_project(page, delete_project):
+    description_text = "Description pipeline project"
+    new_job_link_loc = "//a[@href='newJob']"
+    item_name_field_loc = "//input[@id='name']"
+    pipeline_link_loc = "//div/label/span[text()='Pipeline']"
+    ok_button_loc = "//button[@id='ok-button']"
+    description_text_loc = "//textarea[@name='description']"
+    submit_button_loc= "//button[@name='Submit']"
+    description_content_text = "#description-content"
+
+    page.goto("/")
+    page.locator(new_job_link_loc).click()
+    page.locator(item_name_field_loc).fill(project_name)
+    page.locator(pipeline_link_loc).click()
+    page.locator(ok_button_loc).click()
+    page.locator(description_text_loc).fill(description_text)
+    page.locator(submit_button_loc).click()
+
+    expect(page.locator(description_content_text)).to_have_text(description_text)
