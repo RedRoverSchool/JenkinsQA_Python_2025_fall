814c39775e2b1ef14f2c7140b04531b3eed9ff50
aleksandrastapov <alx.astapov@gmail.com>
2025-12-05 23:58:41 -0800

    Added test

--- tests/test_user_profile_access_aa.py
+++ tests/test_user_profile_access_aa.py
@@ -27,3 +27,43 @@ def test_user_menu_items(page):
 
     for text in expected_common:
         assert any(text in t for t in texts)
+
+def test_user_profile_page(page):
+
+    page.goto("/")
+
+    page.locator(f"a[href='/user/{USER_NAME}']").click()
+
+    user_icon = page.locator("svg.jenkins-avatar").first
+    user_name = page.locator("#side-panel h1")
+    description_link = page.locator("a#description-link")
+    description_textarea = page.locator("textarea[name='description']")
+
+    expect(user_icon).to_be_visible()
+    expect(user_name).to_have_text(re.compile(rf".*{USER_NAME}.*"))
+
+    description_link.click()
+    expect(description_textarea).to_be_visible()
+
+    user_id_label = page.locator("text=Jenkins User ID")
+    expect(user_id_label).to_be_visible()
+    expect(user_id_label).to_have_text(re.compile(rf".*{USER_NAME}.*"))
+
+    items = page.locator("#tasks a")
+
+    expected = [
+        "Profile",
+        "Builds",
+        "My Views",
+        "Account",
+        "Appearance",
+        "Preferences",
+        "Security",
+        "Experiments",
+        "Credentials",
+    ]
+
+    expect(items).to_have_count(len(expected))
+
+    for i, text in enumerate(expected):
+        expect(items.nth(i)).to_have_text(re.compile(rf".*{text}.*"))
