1bc07c403d4500c64ee162ae0634d90ab3fc7c16
Dyb87 <dybin87@gmail.com>
2025-12-07 01:13:16 +0300

    add TC_01.005.01

---
+++ tests/test_able_create_multibranch_pipeline.py
@@ -0,0 +1,18 @@
+import time
+
+
+def test_tc_01_005_01(page):
+    """локатор кнопки new item"""
+    new_item_loc = "a[href='/view/all/newJob']"
+    """Локатор кнопки Multibranch pipeline"""
+    pipeline_multi_loc = "Freestyle project"
+    """Локатор сообщения о пустом поле"""
+    message_loc = "div#itemname-required"
+    message = "» This field cannot be empty, please enter a valid name"
+
+    page.goto("/")
+    page.locator(new_item_loc).click()
+    page.get_by_text(pipeline_multi_loc).click()
+    """Проверка, что текст сообщения об ошибке появляется на странице"""
+    assert  message in page.text_content(message_loc)
+
