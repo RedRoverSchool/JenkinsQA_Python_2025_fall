6ca43f5802ecfa83e8320ba18a18352f62ca98d1
timur <tkdli303@gmail.com>
2025-12-09 18:13:42 -0800

    Add test: test_user_gets_access

---
+++ tests/test_user_gets_access.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import expect
+
+
+def test_user_gets_access(page):
+    page.goto("http://localhost:8080/manage/securityRealm/")
+
+    user_dropdown = page.locator("a[data-dropdown='true'][href='/user/tim']")
+    user_dropdown.hover()
+
+    page.get_by_role("link", name="Account").click()
+
+    page.wait_for_timeout(5000)
+
+    expect(page).to_have_url("/user/tim/account/")
