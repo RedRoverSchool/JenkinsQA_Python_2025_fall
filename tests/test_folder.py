f00c62ae035b565f0e1ad898fb645a5c528c6f5b
IrinaGeybo <irinageybo@gmail.com>
2025-12-10 13:15:58 -0500

    ADD test Verify folder is deleted

---
+++ tests/test_folder.py
@@ -0,0 +1,33 @@
+import random
+from playwright.sync_api import expect
+
+
+def test_delete_folder(page):
+    page.goto("/")
+    created_folder_name = f"TestFolder_{random.randint(0, 100000000)}"
+    new_item_btn_loc = "a[href='/view/all/newJob']"
+    item_name_field_loc = "input[class='jenkins-input']"
+    folder_item_type_text = "Folder"
+    folder_name_text = f"{created_folder_name}"
+    ok_btn_loc = "button[id='ok-button']"
+    dashboard_page_loc = "span[class='jenkins-mobile-hide']"
+    delete_folder_btn_loc = "button[href='/job/{created_folder_name}/doDelete']"
+    confirm_deletion_btn_loc = "button[data-id='ok']"
+    drop_down_btn_loc = f"button[data-href='http://localhost:8080/job/{created_folder_name}/']"
+    page.locator(new_item_btn_loc).click()
+    page.locator(item_name_field_loc).fill(created_folder_name)
+    page.get_by_text(folder_item_type_text, exact=True).click()
+    page.locator(ok_btn_loc).click()
+    page.locator(dashboard_page_loc).click()
+    page.locator(drop_down_btn_loc).click()
+    page.get_by_text("Delete Folder", exact=True).click()
+    page.locator(confirm_deletion_btn_loc).click()
+
+    cell_with_job_name_loc = page.locator("//table[@id='projectstatus']//tr//td/a[@href]")
+    job_name_list = []
+    for i in range(cell_with_job_name_loc.count()):
+        cell = cell_with_job_name_loc.nth(i)
+        job_name_list.append(cell.text_content())
+    print(job_name_list)
+
+    assert created_folder_name not in job_name_list, "Ошибка, указанное значение содержится в списке"
