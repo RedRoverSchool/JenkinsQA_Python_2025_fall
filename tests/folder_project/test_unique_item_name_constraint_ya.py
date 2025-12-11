9072a6c9966bdd92c5ba94181d08ff2237e5a161
alena0505 <alenka1985.0585@gmail.com>
2025-12-12 02:00:11 +0300

    Changed the name of the folder

--- tests/folder_project/test_unique_item_name_constraint_ya.py
+++ tests/folder_project/test_unique_item_name_constraint_ya.py
@@ -4,22 +4,21 @@ def test_unique_item_name_constraint(page):
     """Предусловия"""
     page.goto("/")
     page.locator("a[href='/view/all/newJob']").click()
-    page.locator("#name").fill("Autotests")
+    page.locator("#name").fill("Autotest")
     page.locator("li.com_cloudbees_hudson_plugins_folder_Folder").click()
     page.locator("button[id='ok-button']").click()
     page.locator("#jenkins-head-icon").click()
-    page.locator("a.jenkins-table__link.model-link.inside[href='job/Autotests/']").click()
-    page.locator("a[href='/job/Autotests/newJob']").click()
+    page.locator("a.jenkins-table__link.model-link.inside[href='job/Autotest/']").click()
+    page.locator("a[href='/job/Autotest/newJob']").click()
     page.locator("#name").fill("Test_Project")
     page.locator(".hudson_model_FreeStyleProject").click()
     page.locator("button[id='ok-button']").click()
-    
-    folder_name = "Autotests"
+
+    folder_name = "Autotest"
     existing_project = "Test_Project"
 
     page.goto(f"/job/{folder_name}")
-    page.wait_for_selector("a[href='/job/Autotests/newJob']", timeout=60000)
-    page.click("a[href='/job/Autotests/newJob']")
+    page.click("a[href='/job/Autotest/newJob']")
 
     page.fill("#name", existing_project)
     page.wait_for_timeout(1000)
