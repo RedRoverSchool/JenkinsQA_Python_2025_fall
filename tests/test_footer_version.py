bff941b69bd808eff86f01668d5d3c74dad3e1b5
Pavelbortzov <pavelbortzov@gmail.com>
2025-12-11 16:11:24 +0200

    TC_16.001.01: Added footer version verification

---
+++ tests/test_footer_version.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import expect
+
+
+def test_verify_footer_version(page):
+
+    page.goto("/")
+
+    footer_version_btn = page.locator("button.jenkins-button--tertiary")
+
+    expect(footer_version_btn).to_be_visible()
+
+    expect(footer_version_btn).to_contain_text("Jenkins")
