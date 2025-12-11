e32653e2ac8cefa1341f00cbbc68989b63acf174
timur <tkdli303@gmail.com>
2025-12-10 17:47:46 -0800

    Stabilize MC1 delete cancel test using job row locator

--- tests/test_delete_cancel_mc1.py
+++ tests/test_delete_cancel_mc1.py
@@ -1,24 +1,19 @@
 from playwright.sync_api import expect
 
-
 def test_delete_cancel_mc1(page):
     page.goto("/")
 
     dropdown = page.locator(
-        "button.jenkins-menu-dropdown-chevron[data-href*='/job/MC1/']"
+        "button.jenkins-menu-dropdown-chevron[data-href$='/job/MC1/']"
     )
-
-    expect(dropdown).to_be_visible()
     dropdown.click(force=True)
 
     delete_btn = page.locator(
         "button.jenkins-dropdown__item[href='/job/MC1/doDelete']"
     )
-    expect(delete_btn).to_be_visible()
     delete_btn.click()
 
     cancel_btn = page.locator("button[data-id='cancel']")
-    expect(cancel_btn).to_be_visible()
     cancel_btn.click()
 
     expect(page).to_have_url("/")
