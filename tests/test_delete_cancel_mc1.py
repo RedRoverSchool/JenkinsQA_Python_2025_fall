acc55c144955889ff4c7ee649ed7d66c94136fdd
timur <tkdli303@gmail.com>
2025-12-10 16:57:22 -0800

    Fix delete cancel MC1 test

--- tests/test_delete_cancel_mc1.py
+++ tests/test_delete_cancel_mc1.py
@@ -1,16 +1,13 @@
 from playwright.sync_api import expect
 
-
 def test_delete_cancel_mc1(page):
     page.goto("/")
 
-    page.locator(
-        "button.jenkins-menu-dropdown-chevron[data-href*='/job/MC1/']"
-    ).click(force=True)
+    job_row = page.locator("tr:has-text('MC1')")
 
-    page.locator(
-        "button.jenkins-dropdown__item[href='/job/MC1/doDelete']"
-    ).click()
+    job_row.locator(".jenkins-menu-dropdown-chevron").click(force=True)
+
+    page.locator("button[href='/job/MC1/doDelete']").click()
 
     page.locator("button[data-id='cancel']").click()
 
