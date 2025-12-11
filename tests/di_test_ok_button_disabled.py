3181ce6814e17b39a1d2de4750925c5e2940d404
DariaYasni <d.iasnikova@gmail.com>
2025-12-11 15:10:28 +0200

    add new changes

--- tests/di_test_ok_button_disabled.py
+++ tests/di_test_ok_button_disabled.py
@@ -3,38 +3,38 @@ from playwright.async_api import async_playwright, expect
 
 @pytest.mark.asyncio
 async def test_ok_button_login():
-    # Асинхронный контекст Playwright прямо в тесте
-    async with async_playwright() as p:
-        # Запускаем браузер в headless режиме
-        browser = await p.chromium.launch(headless=True)
-        page = await browser.new_page()
+    browser = None
+    try:
+        async with async_playwright() as p:
 
-        # Переходим на страницу логина Jenkins
-        await page.goto("http://localhost:8080/login")
-
-        # Локаторы
-        ok_button = page.locator("[name='Submit']")
-        username_input = page.locator("#j_username")
-        password_input = page.locator("[name='j_password']")
-
-        # Проверяем, что кнопка видима и включена
-        await expect(ok_button).to_be_visible()
-        assert await ok_button.is_enabled(), "Кнопка должна быть включена изначально"
-
-        # Вводим логин и пароль
-        await username_input.fill("DariaIa")
-        await password_input.fill("Solutions89!")
-
-        # Проверяем, что кнопка остаётся включенной после ввода
-        assert await ok_button.is_enabled(), "Кнопка должна оставаться включенной после ввода"
-
-        # Закрываем браузер
-        await browser.close()
+            browser = await p.chromium.launch(headless=True)
+            page = await browser.new_page()
 
 
+            await page.goto("http://localhost:8080/login")
 
 
+            ok_button = page.locator("[name='Submit']")
+            username_input = page.locator("#j_username")
+            password_input = page.locator("[name='j_password']")
 
 
+            await expect(ok_button).to_be_visible()
+            assert await ok_button.is_enabled(), "Кнопка должна быть включена изначально"
 
 
+            await username_input.fill("DariaIa")
+            await password_input.fill("Solutions89!")
+
+
+            assert await ok_button.is_enabled(), "Кнопка должна оставаться включенной после ввода"
+
+    except Exception as e:
+
+        if browser:
+            page = await browser.new_page()
+            await page.screenshot(path="error_screenshot.png")
+        raise e
+    finally:
+        if browser:
+            await browser.close()
