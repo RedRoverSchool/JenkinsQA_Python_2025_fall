8d8c1057b36f22c3c8c66953ef04370ff1286f0c
DariaYasni <d.iasnikova@gmail.com>
2025-12-12 18:59:17 +0200

    add di_test_login_success.py

--- tests/pipeline_project/di_test_login_sucess.py
+++ tests/pipeline_project/di_test_login_sucess.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 
 def test_login_success():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto("http://localhost:8080/login")
         page.locator("#j_username").fill("DariaIa")
