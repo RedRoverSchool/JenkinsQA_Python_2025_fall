1b57b44ee2f3e33a3f8189841e2485875f296577
DariaYasni <d.iasnikova@gmail.com>
2025-12-18 19:34:14 +0200

    trial test

--- tests/test_di_test_login_sucess.py
+++ tests/test_di_test_login_sucess.py
@@ -1,15 +1,25 @@
+import pytest
 from playwright.sync_api import expect
 
+@pytest.fixture
+def registered_user():
 
-def test_login_success(page, base_url):
+    user = {
+        "username": "DariaIa",
+        "password": "Assistants89!"
+    }
+    return user
+
+
+def test_login_success(page, base_url, registered_user):
 
     page.goto(f"{base_url}/login")
-    page.locator("#j_username").fill("DariaIa")
-    page.locator("input[name='j_password']").fill("Assistants89!")
+
+
+    page.locator("#j_username").fill(registered_user["username"])
+    page.locator("input[name='j_password']").fill(registered_user["password"])
     page.locator("button[name='Submit']").click()
 
-
     page.wait_for_url(base_url + "/")
-
 
     expect(page.locator("a:has-text('All')").first).to_be_visible(timeout=5000)
