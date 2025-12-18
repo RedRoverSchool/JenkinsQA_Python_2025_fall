ca827f14d7b0174feb1ff89c160a8eeeb42b3a5a
DariaYasni <d.iasnikova@gmail.com>
2025-12-18 19:38:34 +0200

    TC_00.000_00_trial_successful_login

--- tests/test_di_test_login_sucess.py
+++ tests/test_di_test_login_sucess.py
@@ -1,6 +1,5 @@
 import pytest
 from playwright.sync_api import expect
-
 @pytest.fixture
 def registered_user():
 
@@ -9,16 +8,10 @@ def registered_user():
         "password": "Assistants89!"
     }
     return user
-
-
 def test_login_success(page, base_url, registered_user):
-
     page.goto(f"{base_url}/login")
-
-
     page.locator("#j_username").fill(registered_user["username"])
     page.locator("input[name='j_password']").fill(registered_user["password"])
     page.locator("button[name='Submit']").click()
-
     page.wait_for_url(base_url + "/")
     expect(page.locator("a:has-text('All')").first).to_be_visible(timeout=5000)
