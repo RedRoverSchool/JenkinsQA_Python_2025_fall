b054ca2f5ac0f16cc2f72224d76175b8afc5313a
DariaYasni <d.iasnikova@gmail.com>
2025-12-18 19:35:24 +0200

    trial test

--- tests/test_di_test_login_sucess.py
+++ tests/test_di_test_login_sucess.py
@@ -21,5 +21,4 @@ def test_login_success(page, base_url, registered_user):
     page.locator("button[name='Submit']").click()
 
     page.wait_for_url(base_url + "/")
-
     expect(page.locator("a:has-text('All')").first).to_be_visible(timeout=5000)
