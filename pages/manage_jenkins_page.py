6e157e9d3bd2abef9c4122cbd2475d573ab13710
Nikolay <qapaur2014@gmail.com>
2025-12-19 19:39:26 +0300

    WIP: start manage jenkins page object

---
+++ pages/manage_jenkins_page.py
@@ -0,0 +1,9 @@
+from pages.base_page import BasePage
+from locators.manage_jenkins_locators import ManageJenkinsLocators
+
+class ManageJenkinsPage(BasePage):
+
+    def __init__(self, page):
+        super().__init__(page)
+
+
