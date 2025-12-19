720673fc85ecc0988e9dc086b3a2311fd4ca7674
DariaYasni <d.iasnikova@gmail.com>
2025-12-19 11:05:37 +0200

    TC_11_1111_11

--- tests/test_di_test_login_success.py
+++ tests/test_di_test_login_success.py
@@ -7,6 +7,7 @@ def registered_user():
         "password": "Assistants89!"
     }
     return user
+
 def test_login_success(page, base_url, registered_user):
     page.goto(f"{base_url}/login")
     page.locator("#j_username").fill(registered_user["username"])
