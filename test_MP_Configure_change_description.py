f5fc744c8b1538b4912169fac6c6bab4bbe8c849
Diana Irgibayeva <d.irgibayeva@a1qa.com>
2025-12-21 14:59:43 +0900

    add test_MP_Configure_change_description.py

---
+++ test_MP_Configure_change_description.py
@@ -0,0 +1,40 @@
+import time
+import random
+
+
+def test_tc_06_003_01(page):
+    page.goto("/")
+
+    name_item = f"Pipeline project-{random.randint(1, 9999)}"
+    description_text = f"Here is a description of {name_item}"
+    """Locators"""
+    new_item_loc = "a[href='/view/all/newJob']"
+    pipeline_multi_loc = "Multibranch pipeline"
+    field_name_loc = "input[name='name']"
+    button_ok_loc = "button[id='ok-button']"
+    description_loc = "textarea[name='_.description']"
+    preview_loc = "a[class='textarea-show-preview']"
+    preview_field_loc = "div.textarea-preview:last-child"
+    button_save_loc= "button[name='Submit']"
+    status_text_loc= "[id='view-message']"
+
+    "Preconditions"
+    page.locator(new_item_loc).click()
+    page.locator(field_name_loc).fill(name_item)
+    page.get_by_text(pipeline_multi_loc).click()
+    page.locator(button_ok_loc).click()
+
+    "Testing"
+    page.locator(description_loc).fill(description_text)
+    page.locator(preview_loc).click()
+    assert page.locator(preview_field_loc).text_content() == description_text
+    page.locator(button_save_loc).click()
+
+
+    assert page.locator(status_text_loc).text_content() == description_text
+
+
+
+
+
+
