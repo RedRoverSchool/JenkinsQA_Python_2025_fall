084b8749159bf778e58e09623415c9a22221ce7c
Diana Irgibayeva <d.irgibayeva@a1qa.com>
2025-12-21 19:33:31 +0900

    add test_MP_Configure_change_description.py

--- test_MP_Configure_change_description.py
+++ test_MP_Configure_change_description.py
@@ -7,6 +7,7 @@ def test_tc_06_003_01(page):
 
     name_item = f"Pipeline project-{random.randint(1, 9999)}"
     description_text = f"Here is a description of {name_item}"
+
     """Locators"""
     new_item_loc = "a[href='/view/all/newJob']"
     pipeline_multi_loc = "Multibranch pipeline"
@@ -14,7 +15,7 @@ def test_tc_06_003_01(page):
     button_ok_loc = "button[id='ok-button']"
     description_loc = "textarea[name='_.description']"
     preview_loc = "a[class='textarea-show-preview']"
-    preview_field_loc = "div.textarea-preview:last-child"
+    preview_field_loc = "div.textarea-preview"
     button_save_loc= "button[name='Submit']"
     status_text_loc= "[id='view-message']"
 
@@ -27,11 +28,12 @@ def test_tc_06_003_01(page):
     "Testing"
     page.locator(description_loc).fill(description_text)
     page.locator(preview_loc).click()
+    page.locator(preview_field_loc).wait_for(state="visible")
     assert page.locator(preview_field_loc).text_content() == description_text
     page.locator(button_save_loc).click()
 
-
     assert page.locator(status_text_loc).text_content() == description_text
+    assert page.get_by_text("Add description").is_visible()
 
 
 
