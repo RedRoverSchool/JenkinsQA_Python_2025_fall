ff7a5b8caae35ba83acd38e50d0929a96a9e17a5
Ekaterina-v8 <ekaterinafilatova022@gmail.com>
2025-12-24 17:28:45 -0500

    commit message

---
+++ tests/test_input_field_accepts_alphanumeric_underscores.py
@@ -0,0 +1,20 @@
+import time
+
+from playwright.sync_api import expect
+def test_validation_valid_characters(page):
+    page.goto("/")
+
+    alphanumeric_characters_underscores = "Jen_test123"
+
+    page.locator('//a[@href="/view/all/newJob"]').click()
+    time.sleep(3)
+
+    item_name_field_loc = page.locator('//input[@id="name"]')
+    item_name_field_loc.fill(alphanumeric_characters_underscores)
+
+    expect(item_name_field_loc).to_be_visible()
+
+
+
+
+
