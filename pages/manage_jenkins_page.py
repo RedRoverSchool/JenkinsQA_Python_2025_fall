93f8220501d96fd38a6f37a492a2aae336b4bd4e
Nikolay <qapaur2014@gmail.com>
2025-12-24 13:22:38 +0300

    remove redundant manage jenkins page object

--- pages/manage_jenkins_page.py
+++
@@ -1,9 +0,0 @@
-from pages.base_page import BasePage
-from locators.manage_jenkins_locators import ManageJenkinsLocators
-
-class ManageJenkinsPage(BasePage):
-
-    def __init__(self, page):
-        super().__init__(page)
-
-
