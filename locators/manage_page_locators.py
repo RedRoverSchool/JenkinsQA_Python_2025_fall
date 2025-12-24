9959f81fcc9cd15c7b6b4b1dee7c854121e81449
Nikolay <qapaur2014@gmail.com>
2025-12-24 12:49:00 +0300

    WIP: prepare manage jenkins page

--- locators/manage_page_locators.py
+++ locators/manage_page_locators.py
@@ -4,3 +4,5 @@ from locators.base_locators import BaseLocators
 class ManagePageLocators(BaseLocators):
 
     SYSTEM_CONFIGURATION_LOC = lambda self, name: f"//dt[normalize-space()='{name}']"
+
+    TOOLS_AND_ACTIONS_ITEM = lambda self, name: f"//dt[normalize-space()='{name}']"
---
+++ tests/manage_jenkins/test_pom_tools_and_actions_block_dublicate.py
@@ -0,0 +1,7 @@
+
+
+
+
+
+def test_smoke():
+    assert True
