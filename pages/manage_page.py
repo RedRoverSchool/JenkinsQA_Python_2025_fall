894656d3df996c7d997f7799e512de963e6899de
Николай Свинцов <151873209+NikolaiSvintsov-QA@users.noreply.github.com>
2025-12-25 11:53:41 +0300

    TC_POM_10.034.08_DUPLICATETC | Manage Jenkins > Visibility and clickability > "Tools and Actions" block content (#413)
    * remove redundant manage jenkins page object
    
    * refactor tools and actions block test to use assertions
    
    * reuse existing system configuration locator

--- pages/manage_page.py
+++ pages/manage_page.py
@@ -8,3 +8,6 @@ class ManageJenkinsPage(BasePage):
     def asd(self, name):
         text = self.get_text(self.locators.SYSTEM_CONFIGURATION_LOC(name))
         return text
+
+    def get_tools_and_actions_item_text(self, name:str)-> str:
+        return self.get_text(self.locators.SYSTEM_CONFIGURATION_LOC(name))
