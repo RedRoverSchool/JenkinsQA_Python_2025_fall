98f62430fa23a456d41f5e41bc318fc3bec69d2d
alena0505 <alenka1985.0585@gmail.com>
2026-01-04 14:57:55 +0300

    TC_01.004.04  Verify job is automatically saved without configuration (#421)
    * Create test_job_auto_saved_without_configuration_ya.py. Add generate_pipeline_name() to ProjectGenerator. Update test file name
    
    * Update test_new_folder_is_empty_by_default_ya.py
    
    Add Endpoints

--- tests/folder_project/test_new_folder_is_empty_by_default_ya.py
+++ tests/folder_project/test_new_folder_is_empty_by_default_ya.py
@@ -1,15 +1,16 @@
 from playwright.sync_api import expect
 from data.enums import ItemType
 from utils.generators.project_generator import ProjectGenerator
+from data.endpoints import Endpoints
 
-def test_tc_01_002_04_new_folder_is_empty_by_default(create_job, base_project_page):
+def test_tc_01_002_04_new_folder_is_empty_by_default(create_job, page):
     """TC_01.002.04 | New Item > Folder > Verify a new folder is empty by default"""
 
     folder_name = ProjectGenerator.generate_folder_name()
     create_job(folder_name, ItemType.FOLDER)
-    base_project_page.go_to_project_page(folder_name)
+    page.goto(Endpoints().JOB_STATUS_PAGE_URL(folder_name))
 
-    expect(base_project_page.page.get_by_text("This folder is empty")).to_be_visible()
+    expect(page.get_by_text("This folder is empty")).to_be_visible()
 
 
 
