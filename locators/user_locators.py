dc9afe41983ed2d896629338f85d08433fe1f7ef
Roman Burlaka <burlakarv@gmail.com>
2026-01-22 21:15:00 +0100

    TC_15.003.04 | User > Delete > Delete User from the User page

--- locators/user_locators.py
+++ locators/user_locators.py
@@ -3,3 +3,5 @@ from locators.base_locators import BaseLocators
 
 class UserLocators(BaseLocators):
     CREATED_USER_LOC = lambda self, name: f"td > a[href='user/{name}/']"
+    DELETE_USER = lambda self, name: f"a[data-url='user/{name.lower()}/doDelete']"
+    CONFIRM_DELETE_USER = "button[data-id= 'ok']"
--- pages/user_page.py
+++ pages/user_page.py
@@ -1,5 +1,17 @@
 from pages.base_page import BasePage
+from locators.user_locators import UserLocators
 
 
 class UserPage(BasePage):
-    pass
+    loc = UserLocators()
+
+    def delete_user(self, name):
+        self.page.click(self.loc.DELETE_USER(name))
+        self.page.click(self.loc.CONFIRM_DELETE_USER)
+
+    def get_user_name(self,loc):
+        try:
+            return loc.get_text()
+
+        except Exception:
+            return "User not exists"
