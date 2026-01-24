c968b0df534e58ff55e5cbbd128c46ce9001740e
Roman Burlaka <burlakarv@gmail.com>
2026-01-24 20:46:26 +0100

    AT test_tc_01_001_06_error_existing_item_name

--- pages/new_item_page.py
+++ pages/new_item_page.py
@@ -1,7 +1,10 @@
+import allure
+
+from locators.base_locators import BaseLocators
 from locators.new_item_locators import NewItemLocators
 from pages.base_page import BasePage
 
-
+loc = NewItemLocators()
 class NewItemPage(BasePage):
 
     def open_new_item_page(self):
@@ -11,3 +14,13 @@ class NewItemPage(BasePage):
     def is_new_item_page_opened(self) -> bool:
         self.wait_for_visible(NewItemLocators.SELECT_ITEM_TYPE_SECTION)
         return self.is_visible(NewItemLocators.SELECT_ITEM_TYPE_SECTION)
+
+    def create_project_with_existing_name(self, name):
+        with allure.step(f"Enter existing item name: {name}"):
+            self.page.fill(loc.NAME_FIELD, name)
+            return self.page.locator(NewItemLocators.ERROR_MESSAGE).inner_text()
+
+
+
+
+
