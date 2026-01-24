01f3dfeca63c5666d003ada56d1461a992a4b075
Roman Burlaka <burlakarv@gmail.com>
2026-01-24 20:58:06 +0100

    AT test_tc_01_001_06_error_existing_item_name

--- tests/new_item/test_error_message_item_name.py
+++ tests/new_item/test_error_message_item_name.py
@@ -13,7 +13,7 @@ class TestErrorMessageItemName:
     generator = ProjectGenerator()
     endpoints = Endpoints()
     new_item_page = NewItemPage
-    item_name = generator.generate_folder_name()
+    item_name = "folder"
 
     @allure.story("Error: existing item name")
     def test_tc_01_001_06_error_existing_item_name(self, create_job, open_page):
@@ -21,4 +21,4 @@ class TestErrorMessageItemName:
         all_job_page = open_page(NewItemPage, self.endpoints.ALL_NEW_JOB_URL)
         error_message = all_job_page.create_project_with_existing_name(job_name)
 
-        assert error_message == f"» A job already exists with the name ‘{job_name}’"
+        assert error_message == f"» A job already exists with the name ‘folder’"
